7204e41cc9785e4d5a78763628fe38cb1ad901b7
bellmayraa <mayraisabelle2006@gmail.com>
2025-06-10 19:58:20 -0300

    Update atv5lista1.py

--- atv5lista1.py
+++ atv5lista1.py
@@ -1,11 +1,25 @@
 #Atividade 5
-lado1 = int(input("Digite o comprimento do lado1 do triangulo"))
-lado2 = int(input("Digite o comprimento do lado2 do triangulo"))
-lado3 = int(input("Digite o comprimento do lado3 do triangulo"))
+def verificaTriangulo(lado1, lado2, lado3):
+    if lado1 == lado2 and lado2 == lado3:
+        print("Triangulo equilatero")
+    elif (lado1 == lado2 and lado2 != lado3) or (lado1 != lado2 and lado2 == lado3):
+        print("É um isóceles")
+    elif lado1 != lado2 and lado2 != lado3 and lado1 != lado3:
+        print("Triangulo escaleno")
 
-if lado1 == lado2 == lado3:
-    print("O lado1 é equilatero")
-elif  lado1 == lado2 == lado3:
-    print("O lado2 é escaleno")
-else: 
-    print("O lado3 é isósceles")    
+def verificaTrianguloComRetorno(lado1, lado2, lado3):
+    if lado1 == lado2 and lado2 == lado3:
+        return "Triangulo equilatero"
+    elif (lado1 == lado2 and lado2 != lado3) or (lado1 != lado2 and lado2 == lado3):
+        return "É um isóceles"
+    elif lado1 != lado2 and lado2 != lado3 and lado1 != lado3:
+        return "Triangulo escaleno"
+
+# Peça ao usuário os comprimentos dos três lados de um triângulo
+lado1 = int(input("Digite o lado 1: "))
+lado2 = int(input("Digite o lado 2: "))
+lado3 = int(input("Digite o lado 3: "))
+# determine se é equilátero, isósceles ou escaleno
+verificaTriangulo(lado1, lado2, lado3)
+resultado = verificaTrianguloComRetorno(lado1, lado2, lado3)
+print(resultado)
